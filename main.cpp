71ed6ce9935cff61c7b3edeb0c1a1d15a2e7f5a9
Enrique Huaman <ehaquique@gmail.com>
2016-04-20 08:49:52 -0500

    Eliminando registros de Merge

--- main.cpp
+++ main.cpp
@@ -22,14 +22,12 @@ int resta(int x, int y)
 	return x-y;
 }
 
-<<<<<<< HEAD
-
 double division(double a, double b){
 	return a/b; // Ricardo
 }
-=======
+
 int multiplicar(int x, int y)
 {
 	return x*y;
 }
->>>>>>> 34a9ac4cac5e539029c2c2e53af64d61f0f0bd59
+
