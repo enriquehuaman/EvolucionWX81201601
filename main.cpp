f87e579bd850c26d9d83dfa0a5e31be7125c5128
unknown <operador@A201-19.upc.edu.pe>
2016-04-20 07:36:15 -0500

    Se actualizo la 4ta linea

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main()
 	printf("Hola Mundo\n");
 	printf("Tercer Cambio");
 	printf("Cambio paralelo\n");	
-	printf("Cuarto Cambio\n");
+	printf("VENDO EMPANADAS\n");
 	printf("Quinto Cambio\n");
 	printf("Cambio de Ricardo\n");
 	return 0;
